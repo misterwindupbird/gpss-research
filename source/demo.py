35bf72d762c0dd24dc04310f504813ef0f6b7e82
Eric Brochu <ebrochu@tableau.com>
2017-03-13 16:16:52 -0700

    Turn the run_experiment_file part of the demo back on.

--- source/demo.py
+++ source/demo.py
@@ -11,7 +11,7 @@ Created April 2013
 import experiment
 import postprocessing
 
-# experiment.run_experiment_file('../experiments/debug/debug_example.py')
+experiment.run_experiment_file('../experiments/debug/debug_example.py')
 
 # To see the outcome of this experiment, look in examples/01-airline_result.txt
 
